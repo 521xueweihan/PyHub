08944bee62877204c2a31a428c6ba99627cc60df
xueweihan <595666367@qq.com>
2017-01-07 00:19:04 +0800

    创建输出层，准备做管理界面

--- pyhub/model.py
+++ pyhub/model.py
@@ -2,15 +2,31 @@
 # -*- coding:utf-8 -*-
 # 
 #   Author  :   XueWeiHan
-#   E-mail  :   595666367@qq.com
 #   Date    :   16/9/30 下午11:05
 #   Desc    :   数据层
 import os
 import sqlite3
 
-DATABASE_PATH = os.path.join(os.path.dirname(__file__), 'test.db')
+from peewee import Model, SqliteDatabase, CharField, TextField, \
+    DateTimeField, IntegerField
 
-conn = sqlite3.connect(DATABASE_PATH)
+DATABASE_PATH = os.path.join(os.path.dirname(__file__), 'test.db')
+database = SqliteDatabase(DATABASE_PATH)
+
+
+class BaseModel(Model):
+    class Meta:
+        database = database
+
+
+class Blog(BaseModel):
+    name = CharField()
+    url = CharField()
+    description = CharField()
+    create_time = DateTimeField()
+    update_time = DateTimeField()
+    status = IntegerField()
+
 # print "Opened database successfully"
 # conn.execute('''CREATE TABLE blog
 #        (id INTEGER PRIMARY KEY,
@@ -19,14 +35,14 @@ conn = sqlite3.connect(DATABASE_PATH)
 #        type        CHAR(10));''')
 # print "Table created successfully"
 
-conn.execute("INSERT INTO blog (name,url,type) \
-      VALUES ('Paul', 'http://www.baidu.com', 'California')")
-
-conn.commit()
-cursor = conn.execute("SELECT id, name, url, type  from blog")
-for row in cursor:
-   print "ID = ", row[0]
-   print "NAME = ", row[1]
-   print "URL = ", row[2]
-   print "TYPE = ", row[3], "\n"
-conn.close()
+# conn.execute("INSERT INTO blog (name,url,type) \
+#       VALUES ('Paul', 'http://www.baidu.com', 'California')")
+#
+# conn.commit()
+# cursor = conn.execute("SELECT id, name, url, type  from blog")
+# for row in cursor:
+#    print "ID = ", row[0]
+#    print "NAME = ", row[1]
+#    print "URL = ", row[2]
+#    print "TYPE = ", row[3], "\n"
+# conn.close()
--- pyhub/server.py
+++ pyhub/server.py
@@ -2,10 +2,11 @@
 # -*- coding:utf-8 -*-
 #
 #   Author  :   XueWeiHan
-#   E-mail  :   595666367@qq.com
 #   Date    :   16/3/24 下午12:43
 #   Desc    :   python-blog-page
 from os import path
+
+from model import Blog
 from flask import Flask, render_template, redirect, url_for
 
 
@@ -16,12 +17,26 @@ app.debug = False
 
 @app.route('/')
 def home():
-    return redirect(url_for('blog'))
-
-
-@app.route('/blog')
-def blog():
     return render_template('blog.html')
+
+
+@app.route('/manage')
+def manage():
+    blogs = Blog.select().where(Blog.status == 1)
+    return render_template('manage.html', blogs=blogs)
+
+@app.route('/manage/create')
+def create():
+    pass
+
+@app.route('/manage/update')
+def update():
+    pass
+
+@app.route('/manage/delete')
+def delete():
+    pass
+
 
 #
 # @app.route('/manage')
